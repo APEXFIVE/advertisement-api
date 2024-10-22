e8a0ebb359762a2c00ba860acb19af25913bb3c3
Wisdom Kwawuvi <wisdomkwawuvi23@gmail.com>
2024-10-22 15:37:33 +0000

    logme in

--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,21 @@
 
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser, getProfile, updateProfile } from '../controllers/users.js'
-import{userAvatarUpload} from "../middlewares/upload.js";
-import { hasPermission,isAuthenticated } from "../middlewares/auth.js";
+import { userAvatarUpload } from "../middlewares/upload.js";
+import { hasPermission, isAuthenticated } from "../middlewares/auth.js";
 
 
 const userRouter = Router();
 
 userRouter.post('/users/register', registerUser);
 
-userRouter.post('/users/login',
- loginUser);
+userRouter.post('/users/login', loginUser);
 
-userRouter.get('/users/me', isAuthenticated,hasPermission('get_profile'), getProfile);
+userRouter.get('/users/me', isAuthenticated, hasPermission('get_profile'), getProfile);
 
 userRouter.post('/user/logout', logoutUser);
 
-userRouter.patch('/users/me', isAuthenticated,hasPermission('update_profile'), userAvatarUpload.single('avatar'),updateProfile);
+userRouter.patch('/users/me', isAuthenticated, hasPermission('update_profile'), userAvatarUpload.single('avatar'), updateProfile);
 
 export default userRouter;
 

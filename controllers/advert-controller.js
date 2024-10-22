914bcf50f82e9b404022bab89e54409ffb1cef72
Andrew Sackey <sackeya24@gmail.com>
2024-10-22 23:35:56 +0000

    search logic

--- controllers/advert-controller.js
+++ controllers/advert-controller.js
@@ -3,57 +3,76 @@ import { addAdvertValidator, updateAdvertVAlidator } from "../validators/advert.
 
 export const addAdverts = async (req, res, next) => {
   try {
-      // validate user input
-      const {error, value} = addAdvertValidator.validate({
-        ...req.body,
-        image: req.file?.filename
-      });
-      if (error) {
-        return res.status(422).json(error);
-      }
-      // write ads to the database
-      await AdvertModel.create(value);
-      // respond to request
-      res.status(201).json("Advert was created!");
+    // validate user input
+    const { error, value } = addAdvertValidator.validate({
+      ...req.body,
+      image: req.file?.filename
+    });
+    if (error) {
+      return res.status(422).json(error);
+    }
+    // write ads to the database
+    await AdvertModel.create(value);
+    // respond to request
+    res.status(201).json("Advert was created!");
   } catch (error) {
     next(error);
   };
 }
 
-export const getAdverts = async (req, res, next ) => {
+export const getAdverts = async (req, res, next) => {
   try {
-      // add filter
-      const {filter = "{}", limit = 10, skip = 0} = req.query;
-      // fetch ads from database
-      const usersCon = await AdvertModel
-      .find(JSON.parse(filter))
+    // Getting query parameters for search filters
+    const { title, category, price, limit = 10, skip = 0 } = req.query;
+
+    // Build a filter object based on the query parameters
+    let filter = {};
+
+    if (title) {
+      //regex for case-insensitive title search
+      filter.title = { $regex: title, $options: 'i' };
+    }
+
+    if (category) {
+      filter.category = category;
+    }
+
+    if (price) {
+      filter.price = price;
+    }
+
+    // Fetch ads from the database with the filter
+    const adverts = await AdvertModel
+      .find(filter)
       .limit(limit)
       .skip(skip);
-      // return response
-      res.status(200).json(usersCon);
+
+    // Return response
+    res.status(200).json(adverts);
   } catch (error) {
-      next(error);
+    next(error);
   }
-}
+};
+
 
 export const getOneAdvert = async (req, res, next) => {
   try {
-      const userCon = await AdvertModel.findById(req.params.id);
-      if (!userCon) {
-          return res.status(404).json({
-              message: "Advert not found"
-          });
-      }
-      res.status(200).json(userCon);
+    const userCon = await AdvertModel.findById(req.params.id);
+    if (!userCon) {
+      return res.status(404).json({
+        message: "Advert not found"
+      });
+    }
+    res.status(200).json(userCon);
   } catch (error) {
-      next(error);
+    next(error);
   }
 }
 
 export const updateAdverts = async (req, res, next) => {
-   try {
+  try {
     // validate the input
-    const {error, value} = updateAdvertVAlidator.validate(req.body);
+    const { error, value } = updateAdvertVAlidator.validate(req.body);
     if (error) {
       return res.status(422).json(error);
     }
@@ -67,10 +86,10 @@ export const updateAdverts = async (req, res, next) => {
       });
     }
     // respond to rquest
-     res.status(200).json('Advert has been updated!');
-   } catch (error) {
+    res.status(200).json('Advert has been updated!');
+  } catch (error) {
     next(error);
-   }
+  }
 }
 
 export const deleteAdvert = async (req, res, next) => {
